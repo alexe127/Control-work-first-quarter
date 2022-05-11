e0ffce60c5a6415fd4bdd78ca5dc1003dc49272d
alexe127 <99885886+alexe127@users.noreply.github.com>
2022-05-12 01:24:34 +0900

    написал метод формирования нового массива и вывод результата

--- task/Program.cs
+++ task/Program.cs
@@ -1,7 +1,6 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
 //длина которых меньше либо равна 3 символа.
 
-string[] array = { "hello", "2", "world", ":-)", "123", "1234", "-2", "computer science", "мир", "труд", "май"  };
 //метод выводит на печать массив
 void printArray(string[] array)
 {
@@ -20,3 +19,23 @@ int countElements(string[] array)
     }
     return count;
 }
+// метод формирует массив из строк, 
+//длина которых меньше либо равна 3 символа
+string[] array3OrLessCharacters(string[] array)
+{
+    string[] newArray = new string[countElements(array)];
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[count] = array[i];
+            count++;
+        }
+    }
+    return newArray;
+}
+
+string[] array = { "hello", "2", "world", ":-)", "123", "1234", "-2", "computer science", "мир", "труд", "май"  };
+string[] resultArray = array3OrLessCharacters(array);
+printArray(resultArray);
