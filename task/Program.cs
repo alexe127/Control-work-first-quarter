587574c5ef56b10e3fd56bb1cc40b7a1b372328d
alexe127 <99885886+alexe127@users.noreply.github.com>
2022-05-12 01:14:35 +0900

    задал массив, написал метод вывода массива

--- task/Program.cs
+++ task/Program.cs
@@ -1,3 +1,11 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
 //длина которых меньше либо равна 3 символа.
 
+string[] array = { "hello", "2", "world", ":-)", "123", "1234", "-2", "computer science", "мир", "труд", "май"  };
+//метод выводит на печать массив
+void printArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+        Console.WriteLine();
+}
